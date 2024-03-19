55a651dea2816c2432eb807a93f8a42970d6c295
EmmaBT <162917122+EmmaBT@users.noreply.github.com>
2024-03-18 19:16:36 -0600

    Update book.model.js

--- src/models/book.model.js
+++ src/models/book.model.js
@@ -5,10 +5,10 @@ import mongoose from "mongoose";
 const booksSchema = mongoose.Schema({
     title: {type: String, required:true},
     ISDN: {type: String, required: true, unique: true},
-    edit: {type: String, required: true},
+    edit: {type: String, required: true}, //editorial
     autor: {type: String, required: true},
-    sipnosis: {type: String, required: true},
-    date: {type: Date, required: true}, //debe de ser de maximo 400 caracteres
+    sipnosis: {type: String, required: true}, //debe de ser de maximo 400 caracteres
+    date: {type: Date, required: true}, 
     lenguage: {type: String, required: true},
     tags: {type: SVGStringList},
     image: { data: Buffer, contentType: String , required: true}
