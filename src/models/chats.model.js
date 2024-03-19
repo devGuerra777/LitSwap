86cc45198ef63f3aca69e06071172a8321329a7e
VOSSOSG1L <163231077+VOSSOSG1L@users.noreply.github.com>
2024-03-18 19:48:07 -0600

    base de chats

--- src/models/chats.model.js
+++ src/models/chats.model.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 //coleecion de chats
 
 const chatSchema = mongoose.Schema({
-    date: {type: Date, required: true}
-})
+    date: {type: Date, required: true},
+});
 
 export default mongoose.model('chats',chatSchema)
