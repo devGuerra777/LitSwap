47b3c10df20b4e434394216138bd89f28fbd4881
VOSSOSG1L <163231077+VOSSOSG1L@users.noreply.github.com>
2024-03-18 19:39:44 -0600

    modelos

---
+++ src/models/swap.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+//coleecion de  intercambios
+
+const swapSchema = mongoose.Schema({
+    requestUser: {type:String, required: true},
+    respondUser: {type:String, required: true},
+    requestBook:{
+        title: {type:String, required: true},
+        ISDN: {type:String, required: true},
+        statusImage: { data: Buffer, contentType: String , required: true}
+    },
+    respondBook:{
+        title: {type:String, required: true},
+        ISDN: {type:String, required: true},
+        statusImage: { data: Buffer, contentType: String , required: true}
+    },
+    swapDate: {type: Date, required: true}
+});
+
+export default mongoose.model('swap', swapSchema)
