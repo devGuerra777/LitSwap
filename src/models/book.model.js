6dbb5ee54e66507866ea18d17a2e07128186ef09
devGuerra777 <ramiro.amador.guerra@gmail.com>
2024-03-29 19:28:04 -0600

    Resolve merge conflicts

--- src/models/book.model.js
+++ src/models/book.model.js
@@ -2,20 +2,13 @@ import mongoose from "mongoose";
 
 //coleccion de libros
 
-<<<<<<< HEAD
 const booksSchema = new mongoose.Schema({
     title: {type: String, required:true},
     ISBN: {type: String, required: true, unique: true},
-=======
-const booksSchema = mongoose.Schema({
-    title: {type: String, required:true},
-    ISDN: {type: String, required: true, unique: true},
->>>>>>> bd9a86ed218b3161c4d2ec243857739e38f00aa4
     edit: {type: String, required: true}, //editorial
     autor: {type: String, required: true},
     sipnosis: {type: String, required: true}, //debe de ser de maximo 400 caracteres
     date: {type: Date, required: true}, 
-<<<<<<< HEAD
     language: {type: String, required: true},
     user:
         {
@@ -28,11 +21,6 @@ const booksSchema = mongoose.Schema({
 },  
 {
     timestamps: true,
-=======
-    lenguage: {type: String, required: true},
-    tags: {type: SVGStringList},
-    image: { data: Buffer, contentType: String , required: true}
->>>>>>> bd9a86ed218b3161c4d2ec243857739e38f00aa4
 });
 
 export default mongoose.model('book',booksSchema)
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //Rutas
 
 import { Router } from "express";
@@ -16,17 +15,3 @@ router.post('/logout', logout);
 router.get('/profile', authRequired, profile);
 
 export default router;
-=======
-import { Router } from "express";
-import {login,register,logout} from "../controllers/auth.controller.js";
-
-const router = Router();
-
-router.post('/register',register)
-
-router.post('/login', login)
-
-router.post('/logout', logout)
-
-export default router
->>>>>>> bd9a86ed218b3161c4d2ec243857739e38f00aa4
